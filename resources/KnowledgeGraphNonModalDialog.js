c6bac7b309cf8817c0af3fc7a548720643b2230b
gesinn-it-gea <gesinn-it-gea@gesinn.it>
2026-07-16 13:34:03 +0200

    refactor(js): fix ESLint violations in KnowledgeGraphNonModalDialog.js
    Apply eslint --fix for mechanical style rules (space-in-parens,
    comma-dangle) and fix the `@license` JSDoc tag name.
    
    Refs #59

--- resources/KnowledgeGraphNonModalDialog.js
+++ resources/KnowledgeGraphNonModalDialog.js
@@ -1,15 +1,15 @@
 /**
  * KnowledgeGraph
  *
- * @licence GPL-2.0-or-later
+ * @license GPL-2.0-or-later
  * @author thomas-topway-it for KM-A
  */
 
-KnowledgeGraphNonModalDialog = (function () {
-	function NonModalDialog(config) {
-		NonModalDialog.super.call(this, config);
+KnowledgeGraphNonModalDialog = ( function () {
+	function NonModalDialog( config ) {
+		NonModalDialog.super.call( this, config );
 	}
-	OO.inheritClass(NonModalDialog, OO.ui.Dialog);
+	OO.inheritClass( NonModalDialog, OO.ui.Dialog );
 
 	// NonModalDialog.static.name = 'myDialogNonModal';
 	NonModalDialog.prototype.initialize = function () {
@@ -17,33 +17,33 @@ KnowledgeGraphNonModalDialog = (function () {
 
 		this.setElementId( 'knowledgegraph-credits' );
 
-		NonModalDialog.super.prototype.initialize.apply(this, arguments);
-		this.content = new OO.ui.PanelLayout({ padded: true, expanded: false });
+		NonModalDialog.super.prototype.initialize.apply( this, arguments );
+		this.content = new OO.ui.PanelLayout( { padded: true, expanded: false } );
 
 		this.content.$element.append(
-			'<p>' + mw.msg('knowledgegraph-credits') + '</p>'
+			'<p>' + mw.msg( 'knowledgegraph-credits' ) + '</p>'
 		);
-		this.content.$element.append(mw.msg('knowledgegraph-credits-list'));
-		this.content.$element.append('<p></p><br />');
-		const closeButton = new OO.ui.ButtonWidget({
-			label: OO.ui.msg('ooui-dialog-process-dismiss'),
-		});
-		closeButton.on('click', function () {
+		this.content.$element.append( mw.msg( 'knowledgegraph-credits-list' ) );
+		this.content.$element.append( '<p></p><br />' );
+		const closeButton = new OO.ui.ButtonWidget( {
+			label: OO.ui.msg( 'ooui-dialog-process-dismiss' )
+		} );
+		closeButton.on( 'click', () => {
 			dialog.close();
-		});
+		} );
 
-		this.content.$element.append(closeButton.$element);
-		this.$body.append(this.content.$element);
+		this.content.$element.append( closeButton.$element );
+		this.$body.append( this.content.$element );
 	};
 	NonModalDialog.prototype.getBodyHeight = function () {
-		return this.content.$element.outerHeight(true);
+		return this.content.$element.outerHeight( true );
 	};
-	
-	function create(params) {
-		return ( new NonModalDialog(params) );
+
+	function create( params ) {
+		return ( new NonModalDialog( params ) );
 	}
 
 	return {
-		create,
+		create
 	};
-})();
+}() );
