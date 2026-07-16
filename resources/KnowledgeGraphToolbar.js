c775d28f9cfbaa0816198ff27215804a0403a4ea
gesinn-it-gea <gesinn-it-gea@gesinn.it>
2026-07-16 13:29:13 +0200

    refactor(js): fix ESLint violations in KnowledgeGraphToolbar.js
    Apply eslint --fix for mechanical style rules and manually resolve the
    rest: fix the `@license` JSDoc tag name and scope an eslint-disable
    for the existing always-true debug condition (behavior preserved as-is).
    
    Refs #55

--- resources/KnowledgeGraphToolbar.js
+++ resources/KnowledgeGraphToolbar.js
@@ -1,109 +1,110 @@
 /**
  * KnowledgeGraph
  *
- * @licence GPL-2.0-or-later
+ * @license GPL-2.0-or-later
  * @author thomas-topway-it for KM-A
  */
 
-KnowledgeGraphToolbar = (function () {
-	function createToolbar(onSelect) {
-		var toolFactory = new OO.ui.ToolFactory();
-		var toolGroupFactory = new OO.ui.ToolGroupFactory();
+KnowledgeGraphToolbar = ( function () {
+	function createToolbar( onSelect ) {
+		const toolFactory = new OO.ui.ToolFactory();
+		const toolGroupFactory = new OO.ui.ToolGroupFactory();
 
-		var toolbar = new OO.ui.Toolbar(toolFactory, toolGroupFactory, {
-			actions: true,
-		});
+		const toolbar = new OO.ui.Toolbar( toolFactory, toolGroupFactory, {
+			actions: true
+		} );
 
-		var toolGroup = [
+		const toolGroup = [
 			{
 				name: 'add-node',
 				icon: 'add',
-				title: mw.msg('knowledgegraph-toolbar-add-node'),
-				onSelect,
+				title: mw.msg( 'knowledgegraph-toolbar-add-node' ),
+				onSelect
 			},
 			{
 				name: 'show-config',
 				icon: 'settings',
-				title: mw.msg('knowledgegraph-toolbar-toggle-config'),
-				onSelect,
+				title: mw.msg( 'knowledgegraph-toolbar-toggle-config' ),
+				onSelect
 			},
 			{
 				name: 'export-graph',
 				icon: 'eye',
-				title: mw.msg('knowledgegraph-toolbar-export-graph'),
-				onSelect,
-			},
+				title: mw.msg( 'knowledgegraph-toolbar-export-graph' ),
+				onSelect
+			}
 		];
 
 		// if (Config.context === 'parserfunction') {
-		if (true) {
-			toolGroup.splice(2, 0, {
+		// eslint-disable-next-line no-constant-condition
+		if ( true ) {
+			toolGroup.splice( 2, 0, {
 				name: 'reload',
 				icon: 'reload',
-				title: mw.msg('knowledgegraph-toolbar-reset-network'),
-				onSelect,
-			});
+				title: mw.msg( 'knowledgegraph-toolbar-reset-network' ),
+				onSelect
+			} );
 		}
 
-		createToolGroup(toolFactory, 'group', toolGroup);
+		createToolGroup( toolFactory, 'group', toolGroup );
 
-		toolbar.setup([
+		toolbar.setup( [
 			{
 				name: 'my-group',
 				// type: "bar",
 				// label: "Create property",
-				include: [{ group: 'group' }],
-			},
-		]);
+				include: [ { group: 'group' } ]
+			}
+		] );
 
 		return toolbar;
 	}
 
-	function createTool(obj, config) {
-		var Tool = function () {
+	function createTool( obj, config ) {
+		const Tool = function () {
 			// Tool.super.apply( this, arguments );
-			Tool.super.call(this, arguments[0], config);
+			Tool.super.call( this, arguments[ 0 ], config );
 
-			OO.ui.mixin.PendingElement.call(this, {});
+			OO.ui.mixin.PendingElement.call( this, {} );
 
-			if (KnowledgeGraphFunctions.getNestedProp(['data', 'disabled'], config)) {
+			if ( KnowledgeGraphFunctions.getNestedProp( [ 'data', 'disabled' ], config ) ) {
 				// this.setPendingElement(this.$element)
 				// this.pushPending();
-				this.setDisabled(true);
+				this.setDisabled( true );
 			}
 
-			if (KnowledgeGraphFunctions.getNestedProp(['data', 'pending'], config)) {
+			if ( KnowledgeGraphFunctions.getNestedProp( [ 'data', 'pending' ], config ) ) {
 				// this.setPendingElement(this.$element)
 				this.pushPending();
 			}
 
 			// @see https://gerrit.wikimedia.org/r/plugins/gitiles/oojs/ui/+/c2805c7e9e83e2f3a857451d46c80231d1658a0f/demos/pages/toolbars.js
 			this.toggled = false;
-			if (config.init) {
-				config.init.call(this);
+			if ( config.init ) {
+				config.init.call( this );
 			}
 		};
 
-		OO.inheritClass(Tool, OO.ui.Tool);
-		OO.mixinClass(Tool, OO.ui.mixin.PendingElement);
+		OO.inheritClass( Tool, OO.ui.Tool );
+		OO.mixinClass( Tool, OO.ui.mixin.PendingElement );
 
 		Tool.prototype.onSelect = function () {
-			if (obj.onSelect) {
-				obj.onSelect.call(this);
+			if ( obj.onSelect ) {
+				obj.onSelect.call( this );
 			} else {
 				this.toggled = !this.toggled;
-				this.setActive(this.toggled);
+				this.setActive( this.toggled );
 			}
 			// Tool.emit( 'updateState' );
 		};
 
 		Tool.prototype.onUpdateState = function () {
 			this.popPending();
-			this.setDisabled(false);
+			this.setDisabled( false );
 		};
 
-		for (var i in obj) {
-			Tool.static[i] = obj[i];
+		for ( const i in obj ) {
+			Tool.static[ i ] = obj[ i ];
 		}
 
 		Tool.static.displayBothIconAndLabel = true;
@@ -111,17 +112,17 @@ KnowledgeGraphToolbar = (function () {
 		return Tool;
 	}
 
-	function createToolGroup(toolFactory, groupName, tools) {
-		tools.forEach(function (tool) {
-			var obj = jQuery.extend({}, tool);
+	function createToolGroup( toolFactory, groupName, tools ) {
+		tools.forEach( ( tool ) => {
+			const obj = jQuery.extend( {}, tool );
 			obj.group = groupName;
-			var config = tool.config ? tool.config : {};
+			const config = tool.config ? tool.config : {};
 			delete obj.config;
-			toolFactory.register(createTool(obj, config));
-		});
+			toolFactory.register( createTool( obj, config ) );
+		} );
 	}
 
 	return {
-		create: createToolbar,
+		create: createToolbar
 	};
-})();
+}() );
