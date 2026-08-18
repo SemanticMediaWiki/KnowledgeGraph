84e04cc0dba0a8bdb868eb4782c2febe2046fdf3
gesinn-it-gea <gesinn-it-gea@gesinn.it>
2026-08-18 21:24:42 +0200

    fix(specialpage): register missing SpecialPageAliases for KnowledgeGraphDesigner
    SpecialPage::getTitleFor('KnowledgeGraphDesigner') fatals with "Did not
    find alias for special page" because no SpecialPageAliases file was ever
    registered in extension.json, breaking KnowledgeGraph::onSidebarBeforeOutput()
    whenever $wgKnowledgeGraphShowSidebarLink is enabled.

---
+++ KnowledgeGraph.i18n.alias.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Aliases for the special pages of the KnowledgeGraph extension.
+ *
+ * @license GPL-2.0-or-later
+ */
+
+$specialPageAliases = [];
+
+/** English */
+$specialPageAliases['en'] = [
+	'KnowledgeGraphDesigner' => [ 'KnowledgeGraphDesigner' ],
+];
